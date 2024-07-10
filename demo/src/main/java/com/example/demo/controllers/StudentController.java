c2ec00b4118ac92e7276ab1770c582ee06782a55
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-07-10 17:16:17 +0700

    Exception Handling - Controller Advice

--- demo/src/main/java/com/example/demo/controllers/StudentController.java
+++ demo/src/main/java/com/example/demo/controllers/StudentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("api/v1/students")
 public class StudentController {
     private final StudentService studentService;
     @Autowired
